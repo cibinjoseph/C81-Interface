ddd016148be10b38460f503ec063e051f6243bd9
cibinjoseph <cibinjoseph92@gmail.com>
2019-05-31 16:00:40 +0530

    Add comments

--- demo.f90
+++ demo.f90
@@ -2,18 +2,27 @@ program demo
   use libC81
   implicit none
 
+  integer, parameter :: rows = 4
+  integer, parameter :: cols = 4
+
   character(len=30) :: airfoil_name
   real, allocatable, dimension(:) :: MaL, MaD, MaM
   real, allocatable, dimension(:) :: AL, AD, AM
   real, allocatable, dimension(:,:) :: CL, CD, CM
   real, dimension(4,4) :: A
 
-   call readC81('Samples/sample1.C81',airfoil_name,MaL,AL,CL,MaD,AD,CD,MaM,AM,CM)
 
-   call writeC81('Samples/sampleOutput.C81',airfoil_name,MaL,AL,CL,MaD,AD,CD,MaM,AM,CM)
+  ! Read airfoil data from C81 file
+  call readC81('Samples/sample1.C81',airfoil_name,MaL,AL,CL,MaD,AD,CD,MaM,AM,CM)
 
-   A=getTable('Samples/sample1.csv',4,4)
-   print*,A
+  ! Write airfoil data to C81 file
+  call writeC81('Samples/sampleOutput.C81',airfoil_name,MaL,AL,CL,MaD,AD,CD,MaM,AM,CM)
+
+  ! Demo of subroutine to read tabular data from csv file to Fortran array
+  ! Works similar to dlmread() from Matlab
+  ! Useful for creating arrays from airfoil data in CSV format
+  A=getTable('Samples/sample1.csv',rows,cols)
+  print*,A
 
 
 end program demo
