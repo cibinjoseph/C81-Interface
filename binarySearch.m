4c2d835fb4513d556779c1fe507c8d795f0431ca
cibinjoseph <cibinjoseph92@gmail.com>
2019-06-17 15:54:01 +0530

    Add test subroutines

--- binarySearch.m
+++ binarySearch.m
@@ -1,14 +1,27 @@
 function indx = binarySearch(A,T)
-L = 0;
-n = length(A);
-R = n;
-while (L < R)
-  indx = floor((L+R)*0.5);
-  if (A(indx) < T)
-    L = indx+1;
+  L = 1;
+  n = length(A);
+  R = n;
+  
+  if (abs(A(n) - T) > eps)
+    while (L < R)
+      indx = floor((L+R)*0.5);
+      if (A(indx) <= T)
+        L = indx+1;
+      else
+        R = indx;
+      end
+    end
+    indx = L-1;
+    L = L-1;
+    if (abs(A(R)-T) < eps)
+      R = L;
+    end
   else
-    R = indx;
+  indx = n;
+  L = n;
+  R = n;
   end
-end
-indx = L-1;
-return;
+L
+R
+  return;
---
+++ getValue.m
@@ -0,0 +1,9 @@
+function val = getValue(A,T)
+  indx = binarySearch(A,T);
+  
+  if (abs(A(indx) - T) > eps)
+    
+  else
+    val = A(indx);
+  endif
+endfunction
