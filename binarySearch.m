43d62ae681ed1eeae236cad03cf59a3f69ebde2b
cibinjoseph <cibinjoseph92@gmail.com>
2019-06-19 18:35:28 +0530

    Correct end cases

--- binarySearch.m
+++ binarySearch.m
@@ -1,27 +1,26 @@
-function indx = binarySearch(A,T)
+function [L,R] = binarySearch(A,S)
   L = 1;
   n = length(A);
   R = n;
-  
-  if (abs(A(n) - T) > eps)
-    while (L < R)
-      indx = floor((L+R)*0.5);
-      if (A(indx) <= T)
-        L = indx+1;
+
+  if (abs(A(n) - S) > eps)  % if last element is not S
+    % Binary search algorithm
+    while ((L ~= R) && (R ~= L+1))
+      i = floor((L+R)*0.5);
+      if (A(i) <= S)
+        L = i+1;
       else
-        R = indx;
+        R = i;
       end
     end
-    indx = L-1;
-    L = L-1;
-    if (abs(A(R)-T) < eps)
+    L = max(L-1,1);
+    if (abs(A(L)-S) <= eps)  % if first element is S
       R = L;
     end
-  else
-  indx = n;
-  L = n;
-  R = n;
+  else  % if last element is S
+    i = n;
+    L = n;
+    R = n;
   end
-L
-R
+  [L R]
   return;
